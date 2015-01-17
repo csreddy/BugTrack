33f5bd1e81c65ed1a3ef84fb451461147bac2ee1
Sudhakar Reddy <sudhakarsjce@gmail.com>
2015-01-17 02:29:26 -0800

    made couple more updated

--- server/api/bug/index.js
+++ server/api/bug/index.js
@@ -2,13 +2,14 @@
 
 var express = require('express');
 var controller = require('./bug.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/count', controller.count)
-router.get('/facets', controller.facets);
+router.get('/facets',  controller.facets);
 router.get('/:id(\\d+)', controller.id);
-router.post('/new', controller.new);
+router.post('/new', auth.ensureAuthenticated, controller.new);
 
 module.exports = router; 
--- server/api/search/index.js
+++ server/api/search/index.js
@@ -2,25 +2,10 @@
 
 var express = require('express');
 var controller = require('./search.controller');
-
+var auth = require('../../auth/auth.service');
 var router = express.Router();
 
-function ensureAuthenticated(req, res, next) {
-    console.log('------------from search ----------------');
-    console.log('req.user', req.user);
-    var username = req.originalUrl.replace('/users/', '');
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        res.send(401, {
-            message: 'Please sign in'
-        });
-    }
-}
 
-router.post('/', ensureAuthenticated, controller.search);
-
-
-
+router.post('/', auth.ensureAuthenticated, controller.search);
 
 module.exports = router;
