793f4210eca9986bf122f155e3e909d181856aec
Sudhakar Reddy <sudhakarsjce@gmail.com>
2015-05-06 18:16:05 -0700

    added quickSearch()

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -6,8 +6,8 @@ var app = angular.module('navbar.controllers', []);
  * hide irrelelvant navbar links when user is logged in
  */
 
-app.controller('navbarCtrl', ['$rootScope', '$scope', '$location', '$window',
-    function($rootScope, $scope, $location, $window) {
+app.controller('navbarCtrl', ['$rootScope', '$scope', '$location', '$window', 'Search', 'Flash',
+    function($rootScope, $scope, $location, $window, Search, Flash) {
 
         $rootScope.navbarUser = $window.localStorage.currentUser;
 
@@ -16,10 +16,23 @@ app.controller('navbarCtrl', ['$rootScope', '$scope', '$location', '$window',
             return page === currentRoute ? 'active' : '';
         };
 
-        $rootScope.quickSearch = function(bugId) {
+        /*  $rootScope.quickSearch = function(bugId) {
             $location.path('/bug/' + bugId);
+        };*/
+
+        $rootScope.quickSearch = function(id) {
+            Search.search({
+                q: 'id:' + id
+            }).success(function(response) {
+                try {
+                    var url = '/' + response[1].content.kind.toLowerCase() + '/' + id;
+                    $location.path(url);
+                } catch (e) {
+                    $location.path('/404');
+                }
+            }).error(function(error) {
+                Flash.addAlert('danger', 'Oops! something went wrong. Reload page and try again');
+            });
         };
-
-
     }
 ]);
